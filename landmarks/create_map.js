98c72bd8846f3391d52c3d655ba1dc329d025883
Thu Cao <thu.cao@tufts.edu>
2017-10-20 01:21:41 -0400

    added polyline, fixed markers to 'this'

--- landmarks/create_map.js
+++ landmarks/create_map.js
@@ -8,14 +8,14 @@ var myOptions = {
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var map;
-var meMarker;
-var landMarkers = {};
-var peopleMarkers = {};
+var me ; //Marker;
+// var landMarkers = {};
+// var peopleMarkers = {};
 var infowindow = new google.maps.InfoWindow();
 var data;
 var people;
 var landmarks;
-var closestLandmark = {loc: me, name: "", dist: 0};
+var closestLandmark;;
 
 function init()
 {
@@ -34,7 +34,9 @@ function getMyLocation() {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			myLat = position.coords.latitude;
 			myLng = position.coords.longitude;
+			alert("Logged In: " + myLat + ", " + myLng);
 			renderMap();
+
 		});
 	}
 	else {
@@ -49,20 +51,33 @@ function renderMap()
 	map.panTo(me);
 	// Create a marker
 	getData();
-
-	var title = "<b>Hello, it's me.</b><BR><BR><b>Closest Landmark: </b>" + closestLandmark.name + "<BR><b>Distance: </b>" + closestLandmark.dist + " miles";
-
-	createMarker(meMarker, me, "me.png", title);
-	renderPolyline(me, closestLandmark.loc);
-
+	var title = "<b>Hello, it's me.</b><BR><BR><b>Closest Landmark: </b>" + closestLandmark.properties.Location_Name + "<BR><b>Distance: </b>" + new google.maps.LatLng(closestLandmark.geometry.coordinates[1] , closestLandmark.geometry.coordinates[0]) + " miles";
+	createMarker(me, "me.png", title);
 }
 
-function renderPolyline(me, loc) {
-	// var loc = new google.maps.LatLng(landmark.geometry.coordinates[1], landmark.geometry.coordinates[0]);
-console.log(loc.lat());
-console.log(loc.lng());
+// Gets data from herokuapp and posts my coords
+function getData(){
+	request.open("POST", "https://defense-in-derpth.herokuapp.com/sendLocation", true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.onreadystatechange = function() {//Call a function when the state changes.
+		if(request.readyState == 4 && request.status == 200) {
+			data = JSON.parse(request.responseText);
+			people = data.people;
+			landmarks = data.landmarks;
+			createPeople();
+			createLandmarks();
+			renderPolyline();
+		}
+	};
+	request.send("login=7UfrTBZr&lat=" + myLat + "&lng=" + myLng);
+}
+
+function renderPolyline() {
+	console.log(closestLandmark.geometry.coordinates[1] + " " + closestLandmark.geometry.coordinates[0]);
+
 	var path = [ {lat: me.lat() , lng: me.lng()},
-          	     {lat: loc.lat(), lng: loc.lng()} ];
+          	     {lat: closestLandmark.geometry.coordinates[1] , lng: closestLandmark.geometry.coordinates[0]} ];
+		     console.log("hello");
 
 	var closestLand = new google.maps.Polyline({
   		path: path,
@@ -75,7 +90,7 @@ console.log(loc.lng());
 	closestLand.setMap(map);
 }
 
-function createMarker(marker, loc, icon, title) {
+function createMarker(loc, icon, title) {
 	marker = new google.maps.Marker({
 		position: loc,
 		title: title,
@@ -85,77 +100,50 @@ function createMarker(marker, loc, icon, title) {
 
 	// Open info window on click of marker
 	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(marker.title);
-		infowindow.open(map, marker);
+		infowindow.setContent(this.title);
+		infowindow.open(map, this);
 	});
 
 }
 
-function getData(){
-	request.open("POST", "https://defense-in-derpth.herokuapp.com/sendLocation", true);
-	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	request.onreadystatechange = function() {//Call a function when the state changes.
-		if(request.readyState == 4 && request.status == 200) {
-			data = JSON.parse(request.responseText);
-			people = data.people;
-			landmarks = data.landmarks;
-			createPeople();
-			createLandmarks();
-		}
-	};
-	request.send("login=7UfrTBZr&lat=" + myLat + "&lng=" + myLng);
-}
-
+// Create markers for people logged in onto herokuapp
 function createPeople() {
 	for (i = 0; i < people.length; i++) {
 		var loc = new google.maps.LatLng(people[i].lat, people[i].lng);
 		var login = people[i].login;
-		var dist = calcDistance(me, loc); // TODO : calc distance from me to this person
+		var dist = calcDistance(me, loc);
 		var title = "<b>Login: </b>" + login + "<BR><b>Distance: </b>" + dist + " miles";
-		createMarker(peopleMarkers[i], loc, "person.png",title);
+		createMarker(loc, "person.png",title);
 	}
 }
 
+// Create markers for all landmarks within one mile of me
 function createLandmarks() {
-	var l = 0;
 	var smallDist = 1;
-	var dist = 0;
-	var loc = new google.maps.LatLng(0,0);
 	for (i = 0; i < landmarks.length; i++) {
-		// TODO : calc distance within 1 mile first before making marker
-		loc = new google.maps.LatLng(landmarks[i].geometry.coordinates[1],landmarks[i].geometry.coordinates[0]);
-		dist = calcDistance(me, loc); // TODO : calc distance from me to this person
-		if (dist <= 1) {
-			var title = landmarks[l].properties.Details;
-			createMarker(landMarkers[l], loc, "landmark.png",title);
-			l++;
-			if (dist < smallDist) {
-				console.log(title);
-				closestLandmark.loc = loc;
-				closestLandmark.name = landmarks[l].properties.Location_Name;
-				closestLandmark.dist = dist;
-				smallDist = dist;
-			}
+		var loc = new google.maps.LatLng(landmarks[i].geometry.coordinates[1],landmarks[i].geometry.coordinates[0]);
+		var dist = calcDistance(me, loc);
+		var title = landmarks[i].properties.Details;
+		createMarker(loc, "landmark.png", title);
+		if (dist < smallDist) {
+			console.log(title);
+			closestLandmark = landmarks[i];
+			smallDist = dist;
 		}
 	}
-}
-
-function toRad(x) {
-    	return x * Math.PI / 180;
 }
 
 // Returns the distance between two points in miles
 function calcDistance(myLoc, otherLoc) {
 
 
-	var dLat = toRad(myLoc.lat() - otherLoc.lat());
-	var dLon = toRad(myLoc.lng() - otherLoc.lng());
+	var dLat = (myLoc.lat() - otherLoc.lat()) * Math.PI / 180;
+	var dLon = (myLoc.lng() - otherLoc.lng()) * Math.PI / 180;
 
 	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-		Math.cos(toRad(myLoc.lat())) *
-		Math.cos(toRad(otherLoc.lat())) *
+		Math.cos(myLoc.lat() * Math.PI / 180) *
+		Math.cos(otherLoc.lat() * Math.PI / 180) *
 		Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
-	//return 6371 * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 	return (12742 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)))/1.609344;
 }
